9ced5d1aa5fd8fdd6333e7914c10f2f01a706b09
xahirah <nakatozahirah@gmail.com>
2023-07-21 00:21:08 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,37 @@
-/*
- * File: 0-isupper.c
- * Auth: Brennan D Baraban
- */
-
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _isupper - Checks for uppercase characters.
- * @c: The character to be checked.
- *
- * Return: 1 if character is uppercase, 0 otherwise.
+ * _isupper - Checks for uppercase character
+ *@c: The character to be checked
+ * Return: 1 (True - uppercase) OR 0 (False - lowecase)
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-
-	else
-		return (0);
+if (c > 64 && c < 91)
+{
+return (1);
 }
+else
+{
+return (0);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
